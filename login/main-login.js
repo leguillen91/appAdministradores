367b28d5f8ecdf2f4e1cb3d294eb44ccd0d15066
Luis Guillen <luisguillenortiz_8@hotmail.com>
2023-05-05 12:17:22 -0600

    renderizando

--- login/main-login.js
+++ login/main-login.js
@@ -1,4 +1,60 @@
 
+const validarAdmin = () => {
+
+    fetch('http://localhost:8888/admin', {
+      method: 'GET',
+      headers: {
+        "Content-Type": "application/json", //MIME Type
+      }
+    })
+    .then((respuesta) => respuesta.json())
+    .then((respuestaAdmin) => {
+      console.log(respuestaAdmin);
+      admin = respuestaAdmin;
+
+
+    let email   =document.getElementById('correo').value;
+    let contraseña =document.getElementById('contraseña').value;
+
+    let expPassword= /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[$@$!%*?&])[A-Za-z\d$@$!%*?&]{8,15}/;
+    let expemail =  /^\w+([.-_+]?\w+)*@\w+([.-]?\w+)*(\.\w{2,10})+$/;
+
+
+
+    
+    if (email==null || email.length==0 || expemail.test(email.value)) {
+        //  alert('email invalido');
+        document.getElementById('correo').style.boxShadow='0 0 5px red';
+            console.log('error');
+             // acceso= false;
+
+        } else if (contraseña==null  || expPassword.test(contraseña.value)) {
+            //  alert('Contraseña invalida');
+            document.getElementById('contraseña').style.boxShadow='0 0 5px red';
+                console.log('error');
+                 // acceso= false;
+              
+      
+        } else if((email == admin[0].correo|| email == admin[1].correo) && (contraseña == admin[0].password|| contraseña == admin[1].password)){
+            alert('Usuario Correcto');
+              //console.log('Usuario Correcto');
+            window.location.href = "http://127.0.0.1:5502/menu/menu.html";
+      
+      
+        } else{
+      
+                console.log('Usuario o contraseña incorrecto');
+            }
+
+ 
+    }); 
+  }
+
+
+
+
+/*
+
 function mostrarPaginaMenu() {
     document.body.style.background='#F6F6F9';
     document.getElementById('seccion-menu').style.display='block';
@@ -47,3 +103,5 @@ function mostrarPaginaOrdenes() {
     document.getElementById('seccion-inicio').style.display='none';
 
 }
+
+*/
---
+++ menu/main-menu.js
@@ -0,0 +1,24 @@
+function mostrarInscripccionEmpresas() {
+
+    window.location.href = "http://127.0.0.1:5502/inscripcionEmpresas/inscripcionEmpresas.html";
+    
+}
+
+
+function mostrarInscripccionProductos() {
+
+window.location.href = "http://127.0.0.1:5502/inscripcionProductos/index.html";
+    
+}
+
+function mostrarPaginaMotoristas() {
+
+window.location.href = "http://127.0.0.1:5502/motorista/index.html";
+    
+}
+
+function mostrarPaginaOrdenes() {
+
+window.location.href = "http://127.0.0.1:5502/ordenes/ordenes.html";
+    
+}
